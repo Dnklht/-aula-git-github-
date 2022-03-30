d3bcba6a3e0ae3bcc385c36236c653ffd25942b9
Thiago Luan Pereira - 42111150 <tlup.09@hotmail.com>
2022-03-29 21:17:57 -0300

    Programa da atividade 02

---
+++ Triangulos/src/aplicacao/Triangulos.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+public class Triangulos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lad1, lad2, lad3;
+		
+		System.out.println("Envie o valor do primeiro lado: ");
+        lad1 = sc.nextInt();
+        System.out.println("Envie o valor do segundo lado: ");
+        lad2 = sc.nextInt();
+        System.out.println("Envie o valor do terceiro lado: ");
+        lad3 = sc.nextInt();
+        
+        if (lad1 == lad2 && lad2 == lad3) {
+        	System.out.println("Equilátero");
+        }
+        else if (lad1 == lad2 || lad1 == lad3 || lad2 == lad3) {
+        	System.out.println("Isósceles");
+        }
+        else {
+        	System.out.println("Escaleno");
+        }
+	}
+
+}
