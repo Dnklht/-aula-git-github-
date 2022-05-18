798712e798df550c9f6d9b4bdd1d93b880684ee5
Thiago Luan Pereira - 42111150 <tlup.09@hotmail.com>
2022-05-17 21:44:30 -0300

    Exercício refatoração 1

---
+++ Prática Refatoração 1/1 ao 3/src/aplicacao/Refatoracao.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+public class Refatoracao {
+
+	public static void main(String[] args) {
+	
+	       Equacao eq = new Equacao(4,-4,1);
+	       eq.calcular();
+	        
+	        
+	    }
+
+	}
